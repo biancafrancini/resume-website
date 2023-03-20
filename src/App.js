1f18fc2f7d5b49df8f050d33b0a63ef90c572fc0
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-03-20 13:25:17 +0000

    init repository

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "./style/index.css";
+
 export const App = () => {
-    return <h1>Stay tuned... 😄</h1>
+    return <h1 className="text-4xl border border-pink-600 w-1/2">Stay tuned... 😄</h1>
 }
