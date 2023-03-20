f5b00448e805a2063d8f2023a86afd97261bc5a7
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-03-20 15:30:10 +0000

    get practise with tsparticles and React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+
 import "./style/index.css";
 
 export const App = () => {
-    return <h1 className="text-4xl border border-pink-600 w-1/2">Stay tuned... 😄</h1>
-}
+  return (
+    <div>
+      <h1> Ciao !! </h1>
+    </div>
+  );
+};
