03b65e09a3d203fa89c036b739080c528b41c26a
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-03-28 18:05:33 +0100

    adjustments to homepage, removed flex

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,71 +7,103 @@ import Bianca from "../images/Bianca.jpeg";
 import "../style/homepage.css";
 
 export const Homepage = () => {
-  const [display, setDisplay] = useState("hidden");
-  const [displayArgs, setDisplayArgs] = useState("hidden")
-  //const [isActive, setIsActive] = useState(false);
+  const [displayArgs, setDisplayArgs] = useState("hidden");
+  const [isActive, setIsActive] = useState(false);
   const navigate = useNavigate();
 
+  const [isEducationActive, setIsEducationActive] = useState(false);
+
+  const showOnClick = () => {
+    if (!isActive) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  };
 
   return (
-    <div className="main-container max-w-xl rounded-lg m-0">
+    <div className="main-container max-w-full rounded-lg m-0 justify-center">
       <div className="flex flex-col items-center py-10">
         <h1 className="mb-6 text-6xl text-gray-900 dark:text-white">
           Bianca Francini
         </h1>
         <h3 className="mb-24 text-gray-500 text-2xl dark:text-gray-400">
-          Junior Full stack Developer
+          Front-end Developer
         </h3>
       </div>
-      <motion.div
-        className="box"
-        onClick={() => {
-          setDisplay(
-            "display-talk text-xl text-blue-600 bg-white p-8 border border-2 border-zinc-500 rounded-3xl"
-          );
-          //setIsActive(true);
-        }}
-        //transition={{ type: "spring", stiffness: 400, damping: 17 }}
-      >
-        <div className={displayArgs}>
-            <Outlet />
+
+      <div className="grid grid-cols-3">
+        {/* Left block */}
+        <div className="">
+          {isEducationActive &&
+            <div className="display-args">
+              asdfasdfasdga
+              {/* <Outlet /> */}
+            </div>
+          }
         </div>
-        <div className="info-container flex max-w-xl">
-          <img
-            src={Bianca}
-            className="Bianca-pic border rounded-full mb-24"
-            alt="Bianca pic profile"
-          />
+
+        {/* Icon */}
+        <div
+          className="box info-container max-w-xl"
+          onClick={showOnClick}
+          // transition={{ type: "spring", stiffness: 400, damping: 17 }}
+        >
+            <img
+              src={Bianca}
+              className="Bianca-pic border rounded-full mb-24"
+              alt="Bianca pic profile"
+            />
+        </div>
+
+        {/* Right block */}
+        {isActive && (
           <motion.div
-            initial={{ x: -500 }}
+            initial={{ x: -100 }}
             animate={{ x: 70 }}
             transition={{
               ease: "ease-in-out",
               duration: 2,
-              x: { duration: 2 },
+              x: { duration: 1 },
             }}
           >
-            <div className={display}>
+            <div className=" display-talk text-xl text-blue-600 bg-white p-8 border border-2 border-zinc-500 rounded-3xl">
               Hi there!
               <br />
               My name is Bianca. Nice to meet you 🙃
               <div className="mt-8 font-bold">
-                <p onClick={() => {
-                  navigate('/techskills');
-                  setDisplayArgs("display-args grow");}}>Skills</p>
+                <p
+                  onClick={() => {
+                    navigate("/techskills");
+                    setDisplayArgs("display-args grow");
+                  }}
+                >
+                  Skills
+                </p>
                 <br />
-                <p onClick={() => {
-                  navigate('/education');
-                  setDisplayArgs("display-args grow");}}>Education</p>
+                <p
+                  onClick={() => {
+                    // navigate("/education");
+                    // setDisplayArgs("display-args grow");
+                    setIsEducationActive(true);
+                  }}
+                >
+                  Education
+                </p>
                 <br />
-                <p onClick={() => {
-                  navigate('/experience');
-                  setDisplayArgs("display-args grow");}}>Experience</p>
+                <p
+                  onClick={() => {
+                    navigate("/experience");
+                    setDisplayArgs("display-args grow");
+                  }}
+                >
+                  Experience
+                </p>
               </div>
             </div>
           </motion.div>
-        </div>
-      </motion.div>
+        )}
+      </div>
     </div>
   );
 };
