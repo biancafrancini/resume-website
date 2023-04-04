ee38eb23ab71f50a5b3a9bc7777ac2c21d9bea35
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-04-04 16:41:53 +0100

    complete education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,6 +1,16 @@
 import { motion } from "framer-motion";
+import "../style/education.css";
 
 export const Education = () => {
+
+  const certifications = [
+    {title: "Full Stack Developer Certificate", school: "Develhope"},
+    {title: "SheCodes Front-end Developer Certifications", school: "SheCodes"},
+    {title: "Postgraduate Specialization in Cultural Heritage Management", institute: "Link Campus University"},
+    {title: "Master Degree in Art History", institute: "University of Siena"}
+    /*{title: "Bachelor Degree in Humanities", school: "University of Siena"}*/
+  ]
+
   return (
     <div>
       <motion.div
@@ -9,10 +19,17 @@ export const Education = () => {
         transition={{
           ease: "ease-in-out",
           duration: 2,
-          x: { duration: 2 },
+          x: { duration: 2},
         }}
       >
-        <div className="picture-side-container">Education</div>
+        <div className="picture-side-container flex flex-col">
+          {certifications && certifications.map((certificate, index) => <div key={index} class="indicator my-4">
+            <span class="indicator-item bg-blue-200 border border-1 border-blue-400 rounded-lg badge-lg pb-4">{certificate.institute ? <i class="fa-solid fa-graduation-cap"></i> : <i class="fa-solid fa-scroll"></i>}</span>
+            <div class="content-text text-black grid h-24 bg-white rounded-md place-items-center">
+              <h4 className="px-4 font-bold">{certificate.title}</h4>
+            </div>
+          </div>)}
+        </div>
       </motion.div>
     </div>
   );
