9eb571a76ffcb4f72e45a0415100eddeabaa2a27
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-10-18 15:57:02 +0100

    add aws icon in TechSkills component, change its style

--- src/utilities/icons.js
+++ src/utilities/icons.js
@@ -1,4 +1,4 @@
-import { FaBootstrap, FaNodeJs, FaSass } from "react-icons/fa";
+import { FaAws, FaBootstrap, FaNodeJs, FaSass } from "react-icons/fa";
 import { BsGit } from "react-icons/bs";
 import {
   SiHtml5,
@@ -17,73 +17,76 @@ import {
 } from "react-icons/si";
 
 export const icons = [
-    {
-      name: "html",
-      iconName: <SiHtml5 />,
-    },
-    {
-      name: "css",
-      iconName: <SiCss3 />,
-    },
-    {
-      name: "sass",
-      iconName: <FaSass />,
-    },
-    {
-      name: "bootstrap",
-      iconName: <FaBootstrap />,
-    },
-    {
-      name: "tailwind",
-      iconName: <SiTailwindcss />,
-    },
-    {
-      name: "git",
-      iconName: <BsGit />,
-    },
-    {
-      name: "js",
-      iconName: <SiJavascript />,
-    },
-    {
-        name: "typescript",
-        iconName: <SiTypescript />,
-    },
-    {
-        name: "react",
-        iconName: <SiReact />,
-    },
-    {
-        name: "motion",
-        iconName: <SiFramer />,
-    },
-    {
-        name: "figma",
-        iconName: <SiFigma />,
-    },
-    {
-      name: "node",
-      iconName: <FaNodeJs />,
-    },
-    {
-      name: "nest",
-      iconName: <SiNestjs />,
-    },
-    {
-        name: "python",
-        iconName: <SiPython />,
-    },
-    {
-      name: "mongo",
-      iconName: <SiMongodb />,
-    },
-    {
-      name: "docker",
-      iconName: <SiDocker />,
-    },
-   
-    {
-      name: "postgres",
-      iconName: <SiPostgresql />,
-    },
-  ];
+  {
+    name: "html",
+    iconName: <SiHtml5 />,
+  },
+  {
+    name: "css",
+    iconName: <SiCss3 />,
+  },
+  {
+    name: "sass",
+    iconName: <FaSass />,
+  },
+  {
+    name: "bootstrap",
+    iconName: <FaBootstrap />,
+  },
+  {
+    name: "tailwind",
+    iconName: <SiTailwindcss />,
+  },
+  {
+    name: "git",
+    iconName: <BsGit />,
+  },
+  {
+    name: "js",
+    iconName: <SiJavascript />,
+  },
+  {
+    name: "typescript",
+    iconName: <SiTypescript />,
+  },
+  {
+    name: "react",
+    iconName: <SiReact />,
+  },
+  {
+    name: "motion",
+    iconName: <SiFramer />,
+  },
+  {
+    name: "figma",
+    iconName: <SiFigma />,
+  },
+  {
+    name: "node",
+    iconName: <FaNodeJs />,
+  },
+  {
+    name: "nest",
+    iconName: <SiNestjs />,
+  },
+  {
+    name: "python",
+    iconName: <SiPython />,
+  },
+  {
+    name: "docker",
+    iconName: <SiDocker />,
+  },
+  {
+    name: "mongo",
+    iconName: <SiMongodb />,
+  },
+  {
+    name: "postgres",
+    iconName: <SiPostgresql />,
+  },
+  {
+    name: "aws",
+    iconName: <FaAws />,
+  },
+];
